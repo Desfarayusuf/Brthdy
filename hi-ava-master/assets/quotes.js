22a71e8338484db13897f95ed0d6e07bc8181ea2
Dsvrys <60470765+Desfarayusuf@users.noreply.github.com>
2020-05-11 14:16:52 +0700

    Add files via upload

---
+++ hi-ava-master/assets/quotes.js
@@ -0,0 +1,10 @@
+// prettier-ignore
+export default [
+    { author: "Me", quotes: "Whatever happens, breathing" },
+    { author: "Me", quotes: "Pam ram" },
+    { author: "Me", quotes: "Hey! I wuv uuuu! UwU" },
+    { author: "standinshd", quotes: "I love you.K" },
+    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "staMendinshd", quotes: "I love you." },
+    { author: "standinshd", quotes: "I'm sorry for loving you." },
+];
